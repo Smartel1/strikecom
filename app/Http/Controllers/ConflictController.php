12825f391b401739fec16500a0d1e8949fd9125d
drews <smartel93@mail.ru>
2018-11-16 23:23:29 +0500

    + api

---
+++ app/Http/Controllers/ConflictController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Conflict;
+use App\Http\Requests\ConflictRequest;
+use Illuminate\Http\Request;
+
+class ConflictController extends Controller
+{
+    public function index()
+    {
+        return Conflict::get();
+    }
+
+    public function store(ConflictRequest $request)
+    {
+        return Conflict::create($request->validated())->toArray();
+    }
+
+    public function update(ConflictRequest $request, $id)
+    {
+        $conflict = Conflict::findOrFail($id)->update($request->validated());
+
+        return $conflict->toArray();
+    }
+
+    public function destroy($id)
+    {
+        $conflict = Conflict::findOrFail($id);
+
+        return $conflict->delete();
+    }
+}
