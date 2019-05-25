a7f0d5d84d17f3ddab87da8f6c647e85d6365206
drews <as@bstd.ru>
2019-05-25 10:03:14 +0500

    add fields to comment resource

--- app/Http/Resources/Comment/CommentResource.php
+++ app/Http/Resources/Comment/CommentResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Comment;
 
 use App\Entities\Comment;
 use App\Entities\Photo;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\Resource;
 
 class CommentResource extends Resource
@@ -11,24 +12,28 @@ class CommentResource extends Resource
     /**
      * Структура комментария
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return array
      */
     public function toArray($request)
     {
-        /** @var $comment Comment*/
+        /** @var $comment Comment */
         $comment = $this;
 
         return [
-            'id'      => $comment->getId(),
-            'user'    => $comment->getUser() ? [
-                'id'     => $comment->getUser()->getId(),
-                'name'   => $comment->getUser()->getName(),
-                'email'  => $comment->getUser()->getEmail()
+            'id'        => $comment->getId(),
+            'user'      => $comment->getUser() ? [
+                'id'        => $comment->getUser()->getId(),
+                'name'      => $comment->getUser()->getName(),
+                'image_url' => $comment->getUser()->getImageUrl(),
+                'email'     => $comment->getUser()->getEmail()
             ] : null,
-            'content' => $comment->getContent(),
-            'photos'  => $comment->getPhotos()
-                ->map(function(Photo $photo){ return $photo->getUrl(); })
+            'content'   => $comment->getContent(),
+            'created_at' => $comment->getCreatedAt(),
+            'photos'    => $comment->getPhotos()
+                ->map(function (Photo $photo) {
+                    return $photo->getUrl();
+                })
                 ->getValues(),
         ];
     }
